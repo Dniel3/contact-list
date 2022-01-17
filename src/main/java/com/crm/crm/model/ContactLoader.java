28f21e1950bf09c0119c7a1516f727acdd816b0c
Dniel3 <dhanielrhamirez@gmail.com>
2022-01-16 22:06:03 -0600

    added script to load Contacts to DB

---
+++ src/main/java/com/crm/crm/model/ContactLoader.java
@@ -0,0 +1,21 @@
+package com.crm.crm.model;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ContactLoader implements CommandLineRunner {
+
+  private final ContactRepository repository;
+
+  @Autowired
+  public ContactLoader(ContactRepository repository) {
+    this.repository = repository;
+  }
+
+  @Override
+  public void run(String... args) throws Exception {
+    this.repository.save(new Contact(args[0], args[1], args[2]));
+  }
+}
