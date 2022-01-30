3bba22d790b2179057e224985dccfb081de14d3e
Dniel3 <dhanielrhamirez@gmail.com>
2022-01-30 17:05:25 -0600

    finished base server for react client

---
+++ src/main/java/com/crm/crm/react/ReactServer.java
@@ -0,0 +1,11 @@
+package com.crm.crm.react;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactServer {
+  public static void main(String[] args) {
+    SpringApplication.run(ReactServer.class, args);
+  }
+}
